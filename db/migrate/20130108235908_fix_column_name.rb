506747e7879c8d18b369f4f1cbe7f84a5b149f30
Sachin Gandhi <gandhisi@gmail.com>
2013-01-09 14:32:53 -0800

    fixing rename migrations on food activity and location activity table

--- db/migrate/20130108235908_fix_column_name.rb
+++ db/migrate/20130108235908_fix_column_name.rb
@@ -5,7 +5,7 @@ class FixColumnName < ActiveRecord::Migration
   #def down
   #end
   def change
-    rename_column :food_activity, :restaurant_details_id, :restaurant_detail_id
-    rename_column :location_activity, :location_details_id, :location_detail_id
+    rename_column :food_activities, :restaurant_details_id, :restaurant_detail_id
+    rename_column :location_activities, :location_details_id, :location_detail_id
   end
 end
