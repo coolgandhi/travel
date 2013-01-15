fd6eeb8a70bcc9231de7aa08c7e26f91b30325af
Hung Luu <hungkienluu@gmail.com>
2013-01-15 11:56:17 -0800

    fixed quick tip field from quite_type in the food activity partial

---
+++ db/schema.rb
@@ -0,0 +1,190 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20130108235908) do
+
+  create_table "activity_time_types", :force => true do |t|
+    t.string   "activity_time_type_id"
+    t.string   "activity_period"
+    t.datetime "created_at",            :null => false
+    t.datetime "updated_at",            :null => false
+  end
+
+  create_table "activity_types", :force => true do |t|
+    t.string   "activity_type_id"
+    t.string   "activity_name"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
+
+  create_table "author_infos", :force => true do |t|
+    t.string   "author_id"
+    t.string   "author_name"
+    t.string   "bithday"
+    t.string   "address1"
+    t.string   "address2"
+    t.string   "address3"
+    t.string   "city"
+    t.string   "country"
+    t.string   "postal_code"
+    t.string   "phone"
+    t.string   "email"
+    t.string   "website"
+    t.string   "twitter_handle"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
+
+  create_table "food_activities", :force => true do |t|
+    t.string   "activity_id"
+    t.string   "restaurant_detail_id"
+    t.string   "description"
+    t.string   "quick_tip"
+    t.string   "duration"
+    t.datetime "created_at",           :null => false
+    t.datetime "updated_at",           :null => false
+  end
+
+  create_table "location_activities", :force => true do |t|
+    t.string   "activity_id"
+    t.string   "location_detail_id"
+    t.string   "description"
+    t.string   "quick_tip"
+    t.string   "duration"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+  end
+
+  create_table "location_details", :force => true do |t|
+    t.string   "location_detail_id"
+    t.string   "name"
+    t.string   "description"
+    t.string   "category"
+    t.string   "phone"
+    t.string   "website"
+    t.string   "open_hours"
+    t.string   "address1"
+    t.string   "address2"
+    t.string   "address3"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+    t.string   "zip"
+    t.string   "latitude"
+    t.string   "longitude"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+  end
+
+  create_table "locations", :force => true do |t|
+    t.string   "location_id"
+    t.string   "place"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+    t.string   "latitude"
+    t.string   "longitude"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "restaurant_details", :force => true do |t|
+    t.string   "restaurant_detail_id"
+    t.string   "name"
+    t.string   "description"
+    t.string   "category"
+    t.string   "phone"
+    t.string   "website"
+    t.string   "open_hours"
+    t.string   "address1"
+    t.string   "address2"
+    t.string   "address3"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+    t.string   "zip"
+    t.string   "latitude"
+    t.string   "longitude"
+    t.datetime "created_at",           :null => false
+    t.datetime "updated_at",           :null => false
+  end
+
+  create_table "transport_activities", :force => true do |t|
+    t.string   "activity_id"
+    t.string   "transport_quick_tips"
+    t.string   "transport_type_id"
+    t.string   "duration"
+    t.string   "source"
+    t.string   "destination"
+    t.datetime "created_at",           :null => false
+    t.datetime "updated_at",           :null => false
+  end
+
+  create_table "transport_types", :force => true do |t|
+    t.string   "transport_type_id"
+    t.string   "transport_name"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+  end
+
+  create_table "traveler_types", :force => true do |t|
+    t.string   "traveler_type_id"
+    t.string   "traveler_type_name"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+  end
+
+  create_table "trip_activities", :force => true do |t|
+    t.string   "trip_id"
+    t.string   "activity_id"
+    t.string   "activity_day"
+    t.integer  "activity_sequence_number"
+    t.string   "activity_type"
+    t.string   "activity_time_type"
+    t.datetime "created_at",               :null => false
+    t.datetime "updated_at",               :null => false
+  end
+
+  create_table "trip_comments", :force => true do |t|
+    t.string   "trip_id"
+    t.string   "comment"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "trip_stats", :force => true do |t|
+    t.string   "trip_id"
+    t.float    "total_rating"
+    t.integer  "numbers_rated"
+    t.integer  "likes"
+    t.integer  "tweets"
+    t.integer  "fb_shared"
+    t.integer  "pinned"
+    t.integer  "emailed"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
+  create_table "trips", :force => true do |t|
+    t.string   "trip_id"
+    t.string   "author_id"
+    t.string   "location_id"
+    t.string   "traveler_type_id"
+    t.string   "trip_name"
+    t.string   "trip_summary"
+    t.string   "duration"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
+
+end
