bde181e72695272864e151c6ef8211789f7543bc
Sachin Gandhi <gandhisi@gmail.com>
2013-03-09 07:31:51 -0800

    prep to install to aws

--- vendor/assets/javascripts/facebook-plugins.js
+++ vendor/assets/javascripts/facebook-plugins.js
@@ -2,7 +2,8 @@
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=371948146237702";
+  js.src = "//connect.facebook.net/en_US/all.js";
+  //"#xfbml=1&appId=371948146237702";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
