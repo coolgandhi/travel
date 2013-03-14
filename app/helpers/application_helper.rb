3efc65468b21a6967130a79a6f8a33f6773c833e
Sachin Gandhi <gandhisi@gmail.com>
2013-03-14 16:29:03 -0700

    fix creation of activities in a new trip

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -155,7 +155,7 @@ module ApplicationHelper
   # index details which image to pick if multiple images
   def select_image_given_image_urls(image_urls, width, index = 0)
     if image_urls.nil? or image_urls == ""
-      return "Image_Missing.png"
+      return "/assets/Image_Missing.png"
     end
     
     y = image_urls.to_s.split(";")
