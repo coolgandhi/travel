28080eb9f80d6344d142aefbe5b90cba12b16502
Sachin Gandhi <gandhisi@gmail.com>
2013-08-29 10:07:46 -0700

    fixing the exception we get in prod when googlebot is looking for apple-touch png file. the emails are very annoying. refer: http://stackoverflow.com/questions/12480497/why-am-i-getting-error-for-apple-touch-icon-precomposed-png/12683605#12683605

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,6 +1,11 @@
 class ErrorsController < ApplicationController
   def error_404
     @not_found_path = params[:not_found]
+    if !@not_found_path.blank? and @not_found_path.include? "apple-touch"
+      respond_to do |format|
+        format.png { render :nothing => true, :status => 404 }
+      end
+    end
   end
 
   def error_500
